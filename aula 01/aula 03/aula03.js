586669dbe0ec3a28e53edac388df24ba6965e5ba
tetebrazribeiro-hash <tetebrazribeiro@gmail.com>
2026-04-07 20:35:02 -0300

    aula02
    até a aula03

---
+++ aula 01/aula 03/aula03.js
@@ -0,0 +1,8 @@
+//Veja abaixo exemplo de como e quando usar as crases
+let nome = 'kaka';
+let idade = 16;
+//Numero usados sem aspas (simples ou duplas),
+
+let mensagem= `Olá, meu nome é ${nome} e tenho ${idade} anos`;
+
+console.log(mensagem);
---
+++ aula 01/aula02/aula02.js
@@ -0,0 +1,13 @@
+let nome = "kaka";
+console.log(nome);
+
+const idade = 16;
+console.log(idade)
+//Console.log responsavel por imprimir na tela os resultados
+let aspasSimples = 'Estamos criando um String com aspas simples'
+//string em resumo são texto que podem ser utilizados no javascript
+// 
+
+let aspasduplas = "String com aspas Duplas";
+//Exemplo de string com aspas duplas.
+
