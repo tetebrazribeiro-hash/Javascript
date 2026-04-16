dac7d12a22e56b399475220c348e4b05c388fac6
tetebrazribeiro-hash <tetebrazribeiro@gmail.com>
2026-04-16 20:34:47 -0300

    trabalho
    java script

---
+++ aula 08/aula08.js
@@ -0,0 +1,12 @@
+//OPERADORES DE CONDIÇÃO IF E ELSE BÁSICO
+
+let idade = 14;
+
+//Caso seja verdadeiro
+if (idade >= 18) {
+    console.log("Você é maior de idade")
+}
+//caso contrário(ñ sendo verdadeiro)
+else {
+    console.log("Você é menor de idade");
+};
