08cb0d297ed2f0dabc4779a75822f7caf1b5fccd
tetebrazribeiro-hash <tetebrazribeiro@gmail.com>
2026-04-30 21:01:21 -0300

    aula13

---
+++ aula11/aula11.js
@@ -0,0 +1,10 @@
+let nota = 10;
+
+if(nota >= 7) {
+    console.log("Aprovado")
+} else if(nota >= 5) {
+    console.log("Recuperação")
+}
+else {
+    console.log("Reprovado");
+}
---
+++ aula12/aula12.js
@@ -0,0 +1,23 @@
+// tipo objeto no javascript
+//Objeto é uma estrutura que amazena informações em formato de chave: valor
+
+const aluno = {
+    nome: "Carlos",
+    idade: 22,
+    curso: "Sistemas de informação"
+};
+
+console.log(aluno.idade);
+
+const Maria = {
+    nome: "Maria Eduarda",
+    sobrenome: "Gomes",
+    matricula: 2103,
+    ativo: true
+};
+
+if(Maria.ativo) {
+    console.log(`Aluno ${Maria.nome} está com a matrícula ativa`);
+} else if(nota.ativo) {
+    console.log(`Aluna ${Maria.nome} não está com a matrícula ativa`);
+}
